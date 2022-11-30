446a48ee8441c316f58f4c830dff07a423e02d63
nfluk <nickylukic@gmail.com>
2022-11-30 11:35:13 +0100

    added navbar & header components + fontawsome

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header-list">
+        <div className="header-list__item"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
