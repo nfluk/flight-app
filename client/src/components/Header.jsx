b4fc49a585cec38273ded7603fef5262d59b7ed0
nfluk <nickylukic@gmail.com>
2022-12-02 18:49:42 +0100

    added dates and people bookings

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faCalendarDays,
+  faHandPointLeft,
   faPerson,
   faPlane,
 } from '@fortawesome/free-solid-svg-icons';
+import { DateRange } from 'react-date-range';
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
+import { format } from 'date-fns';
 import './Header.css';
+import { setDayWithOptions } from 'date-fns/fp';
 
 function Header() {
+  const [showDates, setShowDates] = useState(false);
+  const [date, setDate] = useState([
+    {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: 'selection',
+    },
+  ]);
+
+  const [showPeople, setShowPeople] = useState(false);
+  const [people, setPeople] = useState({
+    adults: 0,
+    children: 0,
+  });
+
+  const handleOption = (traveler, operation) => {
+    setPeople((prev) => {
+      return {
+        ...prev,
+        [traveler]:
+          operation === 'increase'
+            ? people[traveler] + 1
+            : people[traveler] - 1,
+      };
+    });
+  };
+
   return (
     <div className="header">
       <div className="header-container">
@@ -34,11 +67,79 @@ function Header() {
           </div>
           <div className="header-search--item">
             <FontAwesomeIcon icon={faCalendarDays} className="header-icon" />
-            <span className="header-search--text">from date to date</span>
+            <span
+              className="header-search--text"
+              onClick={() => {
+                setShowDates(!showDates);
+              }}
+            >{`${format(date[0].startDate, 'yyyy/MM/dd')} - ${format(
+              date[0].endDate,
+              'yyyy/MM/dd'
+            )}`}</span>
+            {showDates && (
+              <DateRange
+                className="date"
+                editableDateInputs={true}
+                onChange={(item) => setDate([item.selection])}
+                moveRangeOnFirstSelection={false}
+                ranges={date}
+              />
+            )}
           </div>
           <div className="header-search--item">
             <FontAwesomeIcon icon={faPerson} className="header-icon" />
-            <span>adults and/or children</span>
+            <span>{`${people.adults} adults & ${people.children} children `}</span>
+            <div className="options">
+              <div className="option-item">
+                <span className="option-text">Adults</span>
+                <div className="option-counter">
+                  <button
+                    className="option-counter-button"
+                    onClick={() => {
+                      handleOption('adults', 'increase');
+                    }}
+                  >
+                    +
+                  </button>
+                  <span className="option-counter-number">{people.adults}</span>
+                  <button
+                    className="option-counter-button"
+                    onClick={() => {
+                      handleOption('adults', 'decrease');
+                    }}
+                  >
+                    -
+                  </button>
+                </div>
+              </div>
+              <div className="option-item">
+                <span className="option-text">Children</span>
+                <div className="option-counter">
+                  <button
+                    className="option-counter-button"
+                    onClick={() => {
+                      handleOption('children', 'increase');
+                    }}
+                  >
+                    +
+                  </button>
+                  <span className="option-counter-number">
+                    {people.children}
+                  </span>
+                  <button
+                    className="option-counter-button"
+                    onClick={() => {
+                      handleOption('children', 'decrease');
+                    }}
+                  >
+                    -
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="header-search--item">
+            <button className="header--button">Search</button>
           </div>
         </div>
       </div>
