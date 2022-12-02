15f72b7ba21d9b00ebfdf4b8eaddbbce57e72014
nfluk <nickylukic@gmail.com>
2022-12-02 20:05:38 +0100

    minor cleaning up

--- client/src/components/Destinations.jsx
+++ client/src/components/Destinations.jsx
@@ -5,21 +5,21 @@ const Destinations = () => {
     <div className="destinations">
       <div className="popular-destinations">
         <img
-          src="https://www.norwegian.com/globalassets/ip/media/destinations/prague/prg_airport.jpg?w=562"
+          src="https://www.norwegian.com/globalassets/ip/media/destinations/london/lgw-uk-london-1920x620.jpg?w=562"
           alt="popular destination"
         />
         <div className="destination-info">
-          <h1>Prague</h1>
-          <h2>From $49.99</h2>
+          <h1>London</h1>
+          <h2>From $29.99</h2>
         </div>
         <div className="popular-destinations">
           <img
-            src="https://www.norwegian.com/globalassets/ip/media/destinations/prague/prg_airport.jpg?w=562"
+            src="https://www.norwegian.com/globalassets/ip/media/destinations/amsterdam/amsterdam-canal-boats1920x630.jpg?w=562"
             alt="popular destination"
           />
           <div className="destination-info">
-            <h1>London</h1>
-            <h2>From $29.99</h2>
+            <h1>Amsterdam</h1>
+            <h2>From $49.49</h2>
           </div>
         </div>
       </div>
